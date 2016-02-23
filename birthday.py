8072124b3f16fedbf2343837e82a728c995583da
willcampbel <williamcampbell@hanovernorwichschools.org>
2016-02-23 14:31:24 -0500

    Updated from Brython Server: 2/23/2016 2:31:26 PM

--- birthday.py
+++ birthday.py
@@ -1,7 +1,7 @@
 """
 birthday.py
 Author: Will Campbell
-Credit: Eli
+Credit: Eli 
 Assignment:
 
 Your program will ask the user the following questions, in this order:
@@ -44,58 +44,60 @@ month = month_name[todaymonth]
 name = input("Hi, what is your name? ") 
 month = input("Hello "+name+", what was the name of the month you were born in? ")
 year = int(input("And what year were you born in, "+name+"? "))
+
 day = int(input("And the day? ") )
-
-if month == "January":
-    monthnumber = 1
-elif month == "February":
-    monthnumber = 2
-elif month == "March":
-    monthnumber = 3
-elif month == "April":
-    monthnumber = 4
-elif month == "May":
-    monthnumber = 5
-elif month == "June":
-    monthnumber = 6
-elif month == "July":
-    monthnumber = 7
-elif month == "August":
-    monthnumber = 8
-elif month == "September":
-    monthnumber = 9
-elif month == "October":
-    monthnumber = 10
-elif month == "November":
-    monthnumber = 11
-elif month == "December":
-    monthnumber = 12
-
-if 1980 <= year <= 1989: 
-    dec= "the eighties"
-elif 1990 <= year <= 1999:
-    dec= "the nineties"
-elif year < 1980:
-    dec= "the stone age"
-
-if 1 <= monthnumber <= 2: 
-    season = "winter"
-elif 3 <= monthnumber <= 5:
-    season = "spring"
-elif 6 <= monthnumber <= 8:
-    season = "summer"
-elif 9 <= monthnumber <= 11:
-    season = "fall"
-elif monthnumber ==12:
-        season = "fall" 
-"""
-Remember to put in Halloween
-"""
 if month == "October" and day == 31:
     print(+name+ ", you were born on Halloween.")
-
-print(name+", you are a "+season+" baby of the "+dec+".")
-
-
-
-
+elif monthnumber == todaymonth and day == todaydate
+    print("Happy birthday ")
+else:
+    
+    
+    if month == "January":
+        monthnumber = 1
+    elif month == "February":
+        monthnumber = 2
+    elif month == "March":
+        monthnumber = 3
+    elif month == "April":
+        monthnumber = 4
+    elif month == "May":
+        monthnumber = 5
+    elif month == "June":
+        monthnumber = 6
+    elif month == "July":
+        monthnumber = 7
+    elif month == "August":
+        monthnumber = 8
+    elif month == "September":
+        monthnumber = 9
+    elif month == "October":
+        monthnumber = 10
+    elif month == "November":
+        monthnumber = 11
+    elif month == "December":
+        monthnumber = 12
+    
+    if 1980 <= year <= 1989: 
+        dec= "the eighties"
+    elif 1990 <= year <= 1999:
+        dec= "the nineties"
+    elif year < 1980:
+        dec= "the stone age"
+    
+    if 1 <= monthnumber <= 2: 
+        season = "winter"
+    elif 3 <= monthnumber <= 5:
+        season = "spring"
+    elif 6 <= monthnumber <= 8:
+        season = "summer"
+    elif 9 <= monthnumber <= 11:
+        season = "fall"
+    elif monthnumber ==12:
+            season = "fall" 
+            
+    print(name+", you are a "+season+" baby of the "+dec+".")
+    
+    
+    
+    
